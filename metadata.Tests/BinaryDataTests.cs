e9481477265a53ee139b35d67dcb5b3e363e920d
Pivnoyzver <panovpetr2004@gmail.com>
2026-04-10 10:08:18 +0500

    реализация класса BinaryData и модульные тесты для него, в отдельном проекте

---
+++ metadata.Tests/BinaryDataTests.cs
@@ -0,0 +1,136 @@
+using NUnit.Framework;
+using metadata;
+using System;
+using System.Text;
+using System.Linq;
+
+namespace metadata.Tests
+{
+    [TestFixture]
+    public class BinaryDataTests
+    {
+        [Test]
+        public void Serialize_GeneralCase_ShouldReturnByteArray()
+        {
+            // Arrange
+            var payloadType = PayloadType.Text;
+            var payloadString = "HelloWorld";
+            var payloadBytes = Encoding.UTF8.GetBytes(payloadString);
+            var binaryData = new BinaryData(payloadType, payloadBytes);
+
+            // Act
+            var result = binaryData.Serialize();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Length, Is.EqualTo(12 + payloadBytes.Length));
+
+            // Verify the actual payload content
+            var resultPayload = result.AsSpan(12).ToArray();
+            var resultString = Encoding.UTF8.GetString(resultPayload);
+            Assert.That(resultString, Is.EqualTo(payloadString));
+        }
+
+        [Test]
+        public void Serialize_HeaderVerification_ShouldFormCorrectSections()
+        {
+            // Arrange
+            var payloadType = PayloadType.Json;
+            var payloadBytes = new byte[] { 0x01, 0x02, 0x03 };
+            var binaryData = new BinaryData(payloadType, payloadBytes);
+            
+            // Act
+            var result = binaryData.Serialize();
+            
+            // Assert
+            // Section 1: Magic word
+            var magicBytes = Encoding.ASCII.GetBytes("stEG");
+            var resultMagic = result.AsSpan(0, 4).ToArray();
+            Assert.That(resultMagic, Is.EqualTo(magicBytes), "Magic header is incorrect");
+            
+            // Section 2: Length
+            var expectedLength = BitConverter.GetBytes(payloadBytes.Length);
+            var resultLength = result.AsSpan(4, 4).ToArray();
+            Assert.That(resultLength, Is.EqualTo(expectedLength), "Length header is incorrect");
+            
+            // Section 3: DataType
+            var expectedDataType = BitConverter.GetBytes((int)payloadType);
+            var resultDataType = result.AsSpan(8, 4).ToArray();
+            Assert.That(resultDataType, Is.EqualTo(expectedDataType), "DataType header is incorrect");
+            
+            // Section 4: Data itself
+            var resultData = result.AsSpan(12).ToArray();
+            Assert.That(resultData, Is.EqualTo(payloadBytes), "Payload data is incorrect");
+        }
+
+        [Test]
+        public void Deserialize_SimpleCase_ShouldPopulateClassVariables()
+        {
+            // Arrange
+            var payloadType = PayloadType.Binary;
+            var payloadBytes = new byte[] { 0xAA, 0xBB, 0xCC, 0xDD };
+            
+            var serializedData = new byte[12 + payloadBytes.Length];
+            Encoding.ASCII.GetBytes("stEG").CopyTo(serializedData, 0);
+            BitConverter.GetBytes(payloadBytes.Length).CopyTo(serializedData, 4);
+            BitConverter.GetBytes((int)payloadType).CopyTo(serializedData, 8);
+            payloadBytes.CopyTo(serializedData, 12);
+            
+            var binaryData = new BinaryData(PayloadType.Text, new byte[] { });
+            
+            // Act
+            binaryData.Deserialize(serializedData);
+            
+            // Assert
+            Assert.That(binaryData.DataType, Is.EqualTo(payloadType));
+            Assert.That(binaryData.Data, Is.EqualTo(payloadBytes));
+        }
+
+        [Test]
+        public void Deserialize_WithOffset_ShouldFindMagicAndPopulate()
+        {
+            // Arrange
+            var payloadType = PayloadType.File;
+            var payloadBytes = new byte[] { 0xFF };
+            
+            var properSerializedData = new byte[12 + payloadBytes.Length];
+            Encoding.ASCII.GetBytes("stEG").CopyTo(properSerializedData, 0);
+            BitConverter.GetBytes(payloadBytes.Length).CopyTo(properSerializedData, 4);
+            BitConverter.GetBytes((int)payloadType).CopyTo(properSerializedData, 8);
+            payloadBytes.CopyTo(properSerializedData, 12);
+            
+            // Prefix some garbage data
+            var garbage = new byte[] { 0x01, 0x02, 0x03, 0x04 };
+            var dataWithOffset = garbage.Concat(properSerializedData).ToArray();
+            
+            var binaryData = new BinaryData(PayloadType.Text, new byte[0]);
+            
+            // Act
+            binaryData.Deserialize(dataWithOffset);
+            
+            // Assert
+            Assert.That(binaryData.DataType, Is.EqualTo(payloadType));
+            Assert.That(binaryData.Data, Is.EqualTo(payloadBytes), "Should extract proper data sequence ignoring initial garbage");
+        }
+
+        [Test]
+        public void FullCycle_SerializeAndDeserialize_ShouldMaintainDataIntegrity()
+        {
+            // Arrange
+            var initialPayloadType = PayloadType.Text;
+            var originalBytes = Encoding.UTF8.GetBytes("Testing full cycle completeness here");
+            
+            var sourceData = new BinaryData(initialPayloadType, originalBytes);
+            
+            // Act
+            var serialized = sourceData.Serialize();
+            var restoredData = new BinaryData(PayloadType.Binary, new byte[0]); // Initial empty state
+            restoredData.Deserialize(serialized);
+            var reSerialized = restoredData.Serialize();
+            
+            // Assert
+            Assert.That(restoredData.DataType, Is.EqualTo(initialPayloadType));
+            Assert.That(restoredData.Data, Is.EqualTo(originalBytes), "Data integrity compromised during full cycle");
+        }
+    }
+}
--- metadata/BinaryData.cs
+++ metadata/BinaryData.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace metadata;
 
@@ -16,19 +16,43 @@ public enum PayloadType
 
 public class BinaryData
 {
-    public const string Magic = "STEG";
-    public PayloadType DataType { get; set; }
+    private static readonly byte[] Magic = Encoding.ASCII.GetBytes("stEG");
     public int Length => Data.Length;
-    public byte[] Data { get; set; } = [];
+    public PayloadType DataType { get; private set; }
+    public byte[] Data { get; private set; }
+
+    public BinaryData(PayloadType dataType, byte[] data)
+    {
+        DataType = dataType;
+        Data = (byte[])data.Clone();
+    }
 
     public byte[] Serialize()
     {
-        throw new NotImplementedException();
+        var result = new byte[12 + Length];
+        var span = result.AsSpan();
+
+        Magic.CopyTo(span.Slice(0, 4));
+        BitConverter.TryWriteBytes(span.Slice(4, 4), Length);
+        BitConverter.TryWriteBytes(span.Slice(8, 4), (int)DataType);
+        Data.AsSpan().CopyTo(span.Slice(12));
+
+        return result;
     }
 
     public void Deserialize(byte[] data)
     {
-        throw new NotImplementedException();
+        ReadOnlySpan<byte> span = data.AsSpan();
+
+        var startIndex = span.IndexOf(Magic);
+        if (startIndex == -1) throw new ArgumentException("Magic not found");
+
+        var length = BitConverter.ToInt32(span.Slice(startIndex + 4, 4));
+        if (length < 0) throw new ArgumentException("Invalid payload length");
+
+        var dataType = (PayloadType)BitConverter.ToInt32(span.Slice(startIndex + 8, 4));
+
+        this.DataType = dataType;
+        this.Data = span.Slice(startIndex + 12, length).ToArray();
     }
 }
-
