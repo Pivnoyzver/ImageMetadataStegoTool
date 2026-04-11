ffe2a9516ce0c89ffc61dabe0b9aeaf44e11329f
maksik <mask40667@gmail.com>
2026-04-11 13:33:19 +0500

    Классы для работы с png и jpeg

---
+++ metadata/PngImage.cs
@@ -0,0 +1,14 @@
+namespace metadata;
+
+public class PngImage : IImage
+{
+    public byte[] Read()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Write(byte[] package)
+    {
+        throw new System.NotImplementedException();
+    }
+}
