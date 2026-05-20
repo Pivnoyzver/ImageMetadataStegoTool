b7db120f5c7754428fecac3f316aad356f8bf3d6
Pivnoyzver <panovpetr2004@gmail.com>
2026-05-20 20:54:13 +0500

    Разложение классов по папкам

--- metadata.Tests/IImageTests.cs
+++ metadata.Tests/IImageTests.cs
@@ -1,89 +1,88 @@
-namespace metadata.Tests
+namespace metadata.Tests;
+
+[TestFixture]
+public class IImageTests
 {
-    [TestFixture]
-    public class IImageTests
+    private byte[] _pngBytes;
+    private byte[] _jpgBytes;
+
+    [OneTimeSetUp]
+    public void Setup()
     {
-        private byte[] _pngBytes;
-        private byte[] _jpgBytes;
+        // Minimal 1x1 PNG
+        _pngBytes = Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==");
 
-        [OneTimeSetUp]
-        public void Setup()
+        // Minimal 1x1 JPG
+        _jpgBytes = Convert.FromBase64String("/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAUDBAQEAwUEBAQFBQUGBwwIBwcHBw8LCwkMEQ8SEhEPERETFhwXExQaFRERGCEYGh0dHx8fExciJCEeJBweHx7/2wBDAQUFBQcGBw4ICA4eFBEUHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh7/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOElZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD6WooooA//2Q==");
+    }
+
+    private IImage GetImageInstance(string extension, byte[] imageBytes)
+    {
+        return extension.ToLower() switch
         {
-            // Minimal 1x1 PNG
-            _pngBytes = Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==");
+            "png" => new PngImage(imageBytes),
+            "jpg" => new JpgImage(imageBytes),
+            _ => throw new ArgumentException("Unknown extension")
+        };
+    }
 
-            // Minimal 1x1 JPG
-            _jpgBytes = Convert.FromBase64String("/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAUDBAQEAwUEBAQFBQUGBwwIBwcHBw8LCwkMEQ8SEhEPERETFhwXExQaFRERGCEYGh0dHx8fExciJCEeJBweHx7/2wBDAQUFBQcGBw4ICA4eFBEUHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh7/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOElZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD6WooooA//2Q==");
-        }
+    [TestCase("png")]
+    [TestCase("jpg")]
+    public void Write_ValidPackage_ShouldReturnNewImageBytes(string extension)
+    {
+        // Arrange
+        byte[] originalBytes = extension == "png" ? _pngBytes : _jpgBytes;
+        IImage image = GetImageInstance(extension, originalBytes);
+        byte[] package = new byte[] { 0x01, 0x02, 0x03, 0x04 };
 
-        private IImage GetImageInstance(string extension, byte[] imageBytes)
-        {
-            return extension.ToLower() switch
-            {
-                "png" => new PngImage(imageBytes),
-                "jpg" => new JpgImage(imageBytes),
-                _ => throw new ArgumentException("Unknown extension")
-            };
-        }
+        // Act
+        byte[] newImageBytes = image.Write(package);
 
-        [TestCase("png")]
-        [TestCase("jpg")]
-        public void Write_ValidPackage_ShouldReturnNewImageBytes(string extension)
-        {
-            // Arrange
-            byte[] originalBytes = extension == "png" ? _pngBytes : _jpgBytes;
-            IImage image = GetImageInstance(extension, originalBytes);
-            byte[] package = new byte[] { 0x01, 0x02, 0x03, 0x04 };
+        // Assert
+        Assert.That(newImageBytes, Is.Not.Null);
+        Assert.That(newImageBytes, Is.Not.EqualTo(originalBytes));
+        Assert.That(newImageBytes.Length, Is.GreaterThan(originalBytes.Length));
+    }
 
-            // Act
-            byte[] newImageBytes = image.Write(package);
+    [TestCase("png")]
+    [TestCase("jpg")]
+    public void Read_AfterWrite_ShouldReturnSamePackage(string extension)
+    {
+        // Arrange
+        byte[] originalBytes = extension == "png" ? _pngBytes : _jpgBytes;
+        IImage originalImage = GetImageInstance(extension, originalBytes);
+        byte[] package = new byte[] { 0x10, 0x20, 0x30, 0x40, 0x50 };
 
-            // Assert
-            Assert.That(newImageBytes, Is.Not.Null);
-            Assert.That(newImageBytes, Is.Not.EqualTo(originalBytes));
-            Assert.That(newImageBytes.Length, Is.GreaterThan(originalBytes.Length));
-        }
+        byte[] newImageBytes = originalImage.Write(package);
+        IImage newImage = GetImageInstance(extension, newImageBytes);
 
-        [TestCase("png")]
-        [TestCase("jpg")]
-        public void Read_AfterWrite_ShouldReturnSamePackage(string extension)
-        {
-            // Arrange
-            byte[] originalBytes = extension == "png" ? _pngBytes : _jpgBytes;
-            IImage originalImage = GetImageInstance(extension, originalBytes);
-            byte[] package = new byte[] { 0x10, 0x20, 0x30, 0x40, 0x50 };
+        // Act
+        byte[] readPackage = newImage.Read();
 
-            byte[] newImageBytes = originalImage.Write(package);
-            IImage newImage = GetImageInstance(extension, newImageBytes);
+        // Assert
+        Assert.That(readPackage, Is.EqualTo(package));
+    }
 
-            // Act
-            byte[] readPackage = newImage.Read();
+    [TestCase("png")]
+    [TestCase("jpg")]
+    public void MultipleWrites_ShouldOverwriteDataAndNotAccumulate(string extension)
+    {
+        // Arrange
+        byte[] originalBytes = extension == "png" ? _pngBytes : _jpgBytes;
+        IImage originalImage = GetImageInstance(extension, originalBytes);
+        byte[] package1 = new byte[] { 0xAA, 0xBB };
+        byte[] package2 = new byte[] { 0xCC, 0xDD, 0xEE };
 
-            // Assert
-            Assert.That(readPackage, Is.EqualTo(package));
-        }
+        // Act
+        byte[] intermediateImageBytes = originalImage.Write(package1);
+        IImage intermediateImage = GetImageInstance(extension, intermediateImageBytes);
 
-        [TestCase("png")]
-        [TestCase("jpg")]
-        public void MultipleWrites_ShouldOverwriteDataAndNotAccumulate(string extension)
-        {
-            // Arrange
-            byte[] originalBytes = extension == "png" ? _pngBytes : _jpgBytes;
-            IImage originalImage = GetImageInstance(extension, originalBytes);
-            byte[] package1 = new byte[] { 0xAA, 0xBB };
-            byte[] package2 = new byte[] { 0xCC, 0xDD, 0xEE };
+        byte[] finalImageBytes = intermediateImage.Write(package2);
+        IImage finalImage = GetImageInstance(extension, finalImageBytes);
 
-            // Act
-            byte[] intermediateImageBytes = originalImage.Write(package1);
-            IImage intermediateImage = GetImageInstance(extension, intermediateImageBytes);
+        byte[] readPackage = finalImage.Read();
 
-            byte[] finalImageBytes = intermediateImage.Write(package2);
-            IImage finalImage = GetImageInstance(extension, finalImageBytes);
-
-            byte[] readPackage = finalImage.Read();
-
-            // Assert
-            Assert.That(readPackage, Is.EqualTo(package2));
-        }
+        // Assert
+        Assert.That(readPackage, Is.EqualTo(package2));
     }
 }
--- metadata.Tests/PackageTests.cs
+++ metadata.Tests/PackageTests.cs
@@ -1,145 +1,144 @@
 using System.Text;
 
-namespace metadata.Tests
+namespace metadata.Tests;
+
+[TestFixture]
+public class PackageTests
 {
-    [TestFixture]
-    public class PackageTests
+    [Test]
+    public void Serialize_GeneralCase_ShouldReturnByteArray()
     {
-        [Test]
-        public void Serialize_GeneralCase_ShouldReturnByteArray()
-        {
-            // Arrange
-            var payloadString = "HelloWorld";
-            var payloadBytes = Encoding.UTF8.GetBytes(payloadString);
-            var binaryData = new Package(payloadBytes);
+        // Arrange
+        var payloadString = "HelloWorld";
+        var payloadBytes = Encoding.UTF8.GetBytes(payloadString);
+        var binaryData = new Package(payloadBytes);
 
-            // Act
-            var result = binaryData.Serialize();
+        // Act
+        var result = binaryData.Serialize();
 
-            // Assert
-            Assert.That(result, Is.Not.Null);
-            Assert.That(result.Length, Is.EqualTo(16 + payloadBytes.Length));
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Length, Is.EqualTo(16 + payloadBytes.Length));
 
-            // Verify the actual payload content
-            var resultPayload = result.AsSpan(16).ToArray();
-            var resultString = Encoding.UTF8.GetString(resultPayload);
-            Assert.That(resultString, Is.EqualTo(payloadString));
-        }
+        // Verify the actual payload content
+        var resultPayload = result.AsSpan(16).ToArray();
+        var resultString = Encoding.UTF8.GetString(resultPayload);
+        Assert.That(resultString, Is.EqualTo(payloadString));
+    }
 
-        [Test]
-        public void Serialize_HeaderVerification_ShouldFormCorrectSections()
-        {
-            // Arrange
-            var payloadType = DataType.File;
-            var payloadBytes = new byte[] { 0x01, 0x02, 0x03 };
-            var extension = "txt";
-            var extensionBytes = Encoding.UTF8.GetBytes(extension);
-            var binaryData = new Package(payloadBytes, extension);
+    [Test]
+    public void Serialize_HeaderVerification_ShouldFormCorrectSections()
+    {
+        // Arrange
+        var payloadType = DataType.File;
+        var payloadBytes = new byte[] { 0x01, 0x02, 0x03 };
+        var extension = "txt";
+        var extensionBytes = Encoding.UTF8.GetBytes(extension);
+        var binaryData = new Package(payloadBytes, extension);
 
-            // Act
-            var result = binaryData.Serialize();
+        // Act
+        var result = binaryData.Serialize();
 
-            // Assert
-            // Section 1: Magic word
-            var magicBytes = Encoding.ASCII.GetBytes("stEG");
-            var resultMagic = result.AsSpan(0, 4).ToArray();
-            Assert.That(resultMagic, Is.EqualTo(magicBytes), "Magic header is incorrect");
+        // Assert
+        // Section 1: Magic word
+        var magicBytes = Encoding.ASCII.GetBytes("stEG");
+        var resultMagic = result.AsSpan(0, 4).ToArray();
+        Assert.That(resultMagic, Is.EqualTo(magicBytes), "Magic header is incorrect");
 
-            // Section 2: Length
-            var expectedLength = BitConverter.GetBytes(payloadBytes.Length);
-            var resultLength = result.AsSpan(4, 4).ToArray();
-            Assert.That(resultLength, Is.EqualTo(expectedLength), "Length header is incorrect");
+        // Section 2: Length
+        var expectedLength = BitConverter.GetBytes(payloadBytes.Length);
+        var resultLength = result.AsSpan(4, 4).ToArray();
+        Assert.That(resultLength, Is.EqualTo(expectedLength), "Length header is incorrect");
 
-            // Section 3: DataType
-            var expectedDataType = BitConverter.GetBytes((int)payloadType);
-            var resultDataType = result.AsSpan(8, 4).ToArray();
-            Assert.That(resultDataType, Is.EqualTo(expectedDataType), "DataType header is incorrect");
+        // Section 3: DataType
+        var expectedDataType = BitConverter.GetBytes((int)payloadType);
+        var resultDataType = result.AsSpan(8, 4).ToArray();
+        Assert.That(resultDataType, Is.EqualTo(expectedDataType), "DataType header is incorrect");
 
-            // Section 4: FileExtension Length
-            var expectedExtLength = BitConverter.GetBytes(extensionBytes.Length);
-            var resultExtLength = result.AsSpan(12, 4).ToArray();
-            Assert.That(resultExtLength, Is.EqualTo(expectedExtLength), "FileExtension length header is incorrect");
+        // Section 4: FileExtension Length
+        var expectedExtLength = BitConverter.GetBytes(extensionBytes.Length);
+        var resultExtLength = result.AsSpan(12, 4).ToArray();
+        Assert.That(resultExtLength, Is.EqualTo(expectedExtLength), "FileExtension length header is incorrect");
 
-            // Section 5: FileExtension
-            var resultExt = result.AsSpan(16, extensionBytes.Length).ToArray();
-            Assert.That(resultExt, Is.EqualTo(extensionBytes), "FileExtension data is incorrect");
+        // Section 5: FileExtension
+        var resultExt = result.AsSpan(16, extensionBytes.Length).ToArray();
+        Assert.That(resultExt, Is.EqualTo(extensionBytes), "FileExtension data is incorrect");
 
-            // Section 6: Data itself
-            var resultData = result.AsSpan(16 + extensionBytes.Length).ToArray();
-            Assert.That(resultData, Is.EqualTo(payloadBytes), "Payload data is incorrect");
-        }
+        // Section 6: Data itself
+        var resultData = result.AsSpan(16 + extensionBytes.Length).ToArray();
+        Assert.That(resultData, Is.EqualTo(payloadBytes), "Payload data is incorrect");
+    }
 
-        [Test]
-        public void Deserialize_SimpleCase_ShouldPopulateClassVariables()
-        {
-            // Arrange
-            var payloadType = DataType.File;
-            var payloadBytes = new byte[] { 0xAA, 0xBB, 0xCC, 0xDD };
-            var extension = "png";
-            var extensionBytes = Encoding.UTF8.GetBytes(extension);
+    [Test]
+    public void Deserialize_SimpleCase_ShouldPopulateClassVariables()
+    {
+        // Arrange
+        var payloadType = DataType.File;
+        var payloadBytes = new byte[] { 0xAA, 0xBB, 0xCC, 0xDD };
+        var extension = "png";
+        var extensionBytes = Encoding.UTF8.GetBytes(extension);
 
-            var serializedData = new byte[16 + extensionBytes.Length + payloadBytes.Length];
-            Encoding.ASCII.GetBytes("stEG").CopyTo(serializedData, 0);
-            BitConverter.GetBytes(payloadBytes.Length).CopyTo(serializedData, 4);
-            BitConverter.GetBytes((int)payloadType).CopyTo(serializedData, 8);
-            BitConverter.GetBytes(extensionBytes.Length).CopyTo(serializedData, 12);
-            extensionBytes.CopyTo(serializedData, 16);
-            payloadBytes.CopyTo(serializedData, 16 + extensionBytes.Length);
+        var serializedData = new byte[16 + extensionBytes.Length + payloadBytes.Length];
+        Encoding.ASCII.GetBytes("stEG").CopyTo(serializedData, 0);
+        BitConverter.GetBytes(payloadBytes.Length).CopyTo(serializedData, 4);
+        BitConverter.GetBytes((int)payloadType).CopyTo(serializedData, 8);
+        BitConverter.GetBytes(extensionBytes.Length).CopyTo(serializedData, 12);
+        extensionBytes.CopyTo(serializedData, 16);
+        payloadBytes.CopyTo(serializedData, 16 + extensionBytes.Length);
 
-            // Act
-            var binaryData = Package.Deserialize(serializedData);
+        // Act
+        var binaryData = Package.Deserialize(serializedData);
 
-            // Assert
-            Assert.That(binaryData.DataType, Is.EqualTo(payloadType));
-            Assert.That(binaryData.Data, Is.EqualTo(payloadBytes));
-            Assert.That(binaryData.FileExtension, Is.EqualTo(extension));
-        }
+        // Assert
+        Assert.That(binaryData.DataType, Is.EqualTo(payloadType));
+        Assert.That(binaryData.Data, Is.EqualTo(payloadBytes));
+        Assert.That(binaryData.FileExtension, Is.EqualTo(extension));
+    }
 
-        [Test]
-        public void Deserialize_WithOffset_ShouldFindMagicAndPopulate()
-        {
-            // Arrange
-            var payloadType = DataType.File;
-            var payloadBytes = new byte[] { 0xFF };
+    [Test]
+    public void Deserialize_WithOffset_ShouldFindMagicAndPopulate()
+    {
+        // Arrange
+        var payloadType = DataType.File;
+        var payloadBytes = new byte[] { 0xFF };
 
-            var properSerializedData = new byte[16 + payloadBytes.Length];
-            Encoding.ASCII.GetBytes("stEG").CopyTo(properSerializedData, 0);
-            BitConverter.GetBytes(payloadBytes.Length).CopyTo(properSerializedData, 4);
-            BitConverter.GetBytes((int)payloadType).CopyTo(properSerializedData, 8);
-            BitConverter.GetBytes(0).CopyTo(properSerializedData, 12);
-            payloadBytes.CopyTo(properSerializedData, 16);
+        var properSerializedData = new byte[16 + payloadBytes.Length];
+        Encoding.ASCII.GetBytes("stEG").CopyTo(properSerializedData, 0);
+        BitConverter.GetBytes(payloadBytes.Length).CopyTo(properSerializedData, 4);
+        BitConverter.GetBytes((int)payloadType).CopyTo(properSerializedData, 8);
+        BitConverter.GetBytes(0).CopyTo(properSerializedData, 12);
+        payloadBytes.CopyTo(properSerializedData, 16);
 
-            // Prefix some garbage data
-            var garbage = new byte[] { 0x01, 0x02, 0x03, 0x04 };
-            var dataWithOffset = garbage.Concat(properSerializedData).ToArray();
+        // Prefix some garbage data
+        var garbage = new byte[] { 0x01, 0x02, 0x03, 0x04 };
+        var dataWithOffset = garbage.Concat(properSerializedData).ToArray();
 
-            // Act
-            var binaryData = Package.Deserialize(dataWithOffset);
+        // Act
+        var binaryData = Package.Deserialize(dataWithOffset);
 
-            // Assert
-            Assert.That(binaryData.DataType, Is.EqualTo(payloadType));
-            Assert.That(binaryData.Data, Is.EqualTo(payloadBytes), "Should extract proper data sequence ignoring initial garbage");
-            Assert.That(binaryData.FileExtension, Is.Null);
-        }
+        // Assert
+        Assert.That(binaryData.DataType, Is.EqualTo(payloadType));
+        Assert.That(binaryData.Data, Is.EqualTo(payloadBytes), "Should extract proper data sequence ignoring initial garbage");
+        Assert.That(binaryData.FileExtension, Is.Null);
+    }
 
-        [Test]
-        public void FullCycle_SerializeAndDeserialize_ShouldMaintainDataIntegrity()
-        {
-            // Arrange
-            var initialPayloadType = DataType.File;
-            var originalBytes = Encoding.UTF8.GetBytes("Testing full cycle completeness here");
-            var extension = "docx";
+    [Test]
+    public void FullCycle_SerializeAndDeserialize_ShouldMaintainDataIntegrity()
+    {
+        // Arrange
+        var initialPayloadType = DataType.File;
+        var originalBytes = Encoding.UTF8.GetBytes("Testing full cycle completeness here");
+        var extension = "docx";
 
-            var sourceData = new Package(originalBytes, extension);
+        var sourceData = new Package(originalBytes, extension);
 
-            // Act
-            var serialized = sourceData.Serialize();
-            var restoredData = Package.Deserialize(serialized);
+        // Act
+        var serialized = sourceData.Serialize();
+        var restoredData = Package.Deserialize(serialized);
 
-            // Assert
-            Assert.That(restoredData.DataType, Is.EqualTo(initialPayloadType));
-            Assert.That(restoredData.Data, Is.EqualTo(originalBytes), "Data integrity compromised during full cycle");
-            Assert.That(restoredData.FileExtension, Is.EqualTo(extension), "FileExtension compromised during full cycle");
-        }
+        // Assert
+        Assert.That(restoredData.DataType, Is.EqualTo(initialPayloadType));
+        Assert.That(restoredData.Data, Is.EqualTo(originalBytes), "Data integrity compromised during full cycle");
+        Assert.That(restoredData.FileExtension, Is.EqualTo(extension), "FileExtension compromised during full cycle");
     }
 }
